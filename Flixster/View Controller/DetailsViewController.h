166bf12aeb07467db6eaf3be2486fbfdea6cd098
Ginger Dudley <gingerdudley@fb.com>
2018-06-27 22:57:42 -0700

    added detail page

---
+++ Flixster/View Controller/DetailsViewController.h
@@ -0,0 +1,15 @@
+//
+//  DetailsViewController.h
+//  Flixster
+//
+//  Created by Ginger Dudley on 6/27/18.
+//  Copyright © 2018 Ginger Dudley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailsViewController : UIViewController
+
+@property (nonatomic, strong) NSDictionary *movie;
+
+@end
